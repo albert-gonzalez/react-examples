8d1f14c58f3ceb87b2a93f72164745df1a0a31b2
vacilone <albert.glez.mld@gmail.com>
2018-04-01 23:02:58 +0200

    removed button constructor

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,13 +3,6 @@ import PropTypes from 'prop-types';
 import './button.css';
 
 class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false
-    };
-  }
-
   render() {
     return (
       <button
